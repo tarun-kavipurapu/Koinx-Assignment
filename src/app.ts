a682666b01558d7d8be154f19e45a10f1bc3c02e
Tarun <tarunsharmakavipurapu@gmail.com>
2024-10-10 07:14:51 +0530

    logger added

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import { errorHandler } from './middlewares/error.middleware';
 import { rateLimiter } from './middlewares/rateLimit.middleware';
 import cronJob from './background-jobs/cryptoJob';
 import cryptoRouter from './routes/crypto.router';
+import morganMiddleware from './logger/morgon.logger';
 
 const app = express();
 const httpServer = http.createServer(app);
 
 // Middleware to parse JSON requests
 app.use(express.json());
+app.use(morganMiddleware);
 
 // Apply rate limiting middleware globally
 app.use(rateLimiter);
@@ -21,6 +23,7 @@ cronJob;
 app.use('/', cryptoRouter);
 
 // Error handler middleware (should be last)
+//@ts-ignore
 app.use(errorHandler);
 
 export { httpServer };
